043f3a06038822e80eaad3a0b9d382545cde21d3
JustSad <44894900+JustSad@users.noreply.github.com>
2019-01-05 05:08:33 -0800

    Add files via upload

---
+++ me.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+const developers = ["335411700251623444"]
+const adminprefix = "+";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/JustSad");
+      message.channel.send(`**Status You ${argresult} **`)
+}
+});
+
+
+
+
+client.login("MzM1NDExNzAwMjUxNjIzNDQ0.Dw6RIw.IP80UvVYlfCKmzh7LRo3U24mkbY");
